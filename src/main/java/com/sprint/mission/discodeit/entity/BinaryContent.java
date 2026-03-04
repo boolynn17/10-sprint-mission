ec4939cc93c4b8f8de0381008ccdf05b44ff8e4a
박린 <lynnpark2003@ehwa.ac.kr>
2026-03-04 18:31:04 +0900

    feat: JPA 및 PostgreSQL 의존성 추가

--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -9,22 +9,22 @@ import java.util.UUID;
 @Getter
 public class BinaryContent implements Serializable {
 
-  private static final long serialVersionUID = 1L;
-  private UUID id;
-  private Instant createdAt;
-  //
-  private String fileName;
-  private Long size;
-  private String contentType;
-  private byte[] bytes;
-
-  public BinaryContent(String fileName, Long size, String contentType, byte[] bytes) {
-    this.id = UUID.randomUUID();
-    this.createdAt = Instant.now();
+    private static final long serialVersionUID = 1L;
+    private UUID id;
+    private Instant createdAt;
     //
-    this.fileName = fileName;
-    this.size = size;
-    this.contentType = contentType;
-    this.bytes = bytes;
-  }
+    private String fileName;
+    private Long size;
+    private String contentType;
+    private byte[] bytes;
+
+    public BinaryContent(String fileName, Long size, String contentType, byte[] bytes) {
+        this.id = UUID.randomUUID();
+        this.createdAt = Instant.now();
+        //
+        this.fileName = fileName;
+        this.size = size;
+        this.contentType = contentType;
+        this.bytes = bytes;
+    }
 }
