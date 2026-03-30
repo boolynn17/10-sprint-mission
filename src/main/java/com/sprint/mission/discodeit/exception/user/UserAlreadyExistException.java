df1fa8d41306a23f6e6ae45975c1d5bd3435c7ad
박린 <lynnpark2003@ehwa.ac.kr>
2026-03-30 10:07:39 +0900

    feat: User 메인 예외 클래스 및 구제적인 예외 클래스 정의

--- src/main/java/com/sprint/mission/discodeit/exception/user/UserAlreadyExistException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/user/UserAlreadyExistException.java
@@ -1,4 +1,23 @@
 package com.sprint.mission.discodeit.exception.user;
 
-public class UserAlreadyExistException {
+import com.sprint.mission.discodeit.exception.ErrorCode;
+
+import java.util.Map;
+
+public class UserAlreadyExistException extends UserException {
+
+    // 기본 생성자
+    public UserAlreadyExistException(ErrorCode errorCode, String value) {
+        super(errorCode, Map.of("duplicateValue", value));
+    }
+
+    // 이메일 중복
+    public static UserAlreadyExistException email(String email) {
+        return new UserAlreadyExistException(ErrorCode.DUPLICATE_EMAIL, email);
+    }
+
+    // 유저명 중복
+    public static UserAlreadyExistException username(String username) {
+        return new UserAlreadyExistException(ErrorCode.DUPLICATE_USERNAME, username);
+    }
 }
