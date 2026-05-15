381c9e5915da22af1ef848de0aaff478c957eb01
박린 <lynnpark2003@ehwa.ac.kr>
2026-05-15 16:29:33 +0900

    fix: DiscodeitUserDeatilsService 클래스명 오타 수정

--- src/main/java/com/sprint/mission/discodeit/security/DiscodeitUserDetailsService.java
+++ src/main/java/com/sprint/mission/discodeit/security/DiscodeitUserDetailsService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class DiscodeitUserDetailService implements UserDetailsService {
+public class DiscodeitUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
