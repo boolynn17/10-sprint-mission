8a9ff995d115075362955a3f0415c63cab7ebf9d
박린 <lynnpark2003@ehwa.ac.kr>
2026-05-15 15:34:15 +0900

    feat: 사용자 정보 조회를 위한 API 정의

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.api.AuthApi;
-import com.sprint.mission.discodeit.service.AuthService;
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
@@ -14,12 +16,16 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/auth")
 public class AuthController implements AuthApi {
 
-  private final AuthService authService;
-
   @GetMapping("/csrf-token")
   public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
     String tokenValue = csrfToken.getToken();
     log.debug("CSRF 토큰 요청: {}", tokenValue);
     return ResponseEntity.noContent().build();
+  }
+
+  @GetMapping("/me")
+  public ResponseEntity<UserDto> getMe(
+          @AuthenticationPrincipal DiscodeitUserDetails userDetails) {
+    return ResponseEntity.ok(userDetails.getUserDto());
   }
 }
