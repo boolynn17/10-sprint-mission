b160f67e7cdabab87fe70450c4dede1ad9e29cf8
박린 <lynnpark2003@ehwa.ac.kr>
2026-05-19 11:04:31 +0900

    fix: DB 연결 오류 해결 및 .yaml 파일 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -125,3 +125,6 @@ ALTER TABLE read_statuses
         FOREIGN KEY (channel_id)
             REFERENCES channels (id)
             ON DELETE CASCADE;
+
+ALTER TABLE users
+    ADD COLUMN role varchar(20) NOT NULL DEFAULT 'USER';
