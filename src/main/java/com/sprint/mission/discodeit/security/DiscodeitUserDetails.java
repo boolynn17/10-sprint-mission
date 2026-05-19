760107988966fb6ff25aee6e2ed9583800e4adcd
박린 <lynnpark2003@ehwa.ac.kr>
2026-05-19 16:27:13 +0900

    feat: 세션 동일성 보장을 위한 equals() 및 hascode() 메소드 오버라이딩

--- src/main/java/com/sprint/mission/discodeit/security/DiscodeitUserDetails.java
+++ src/main/java/com/sprint/mission/discodeit/security/DiscodeitUserDetails.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @RequiredArgsConstructor
@@ -31,4 +32,17 @@ public class DiscodeitUserDetails implements UserDetails {
     public String getUsername() {
         return userDto.username();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DiscodeitUserDetails that)) return false;
+        return Objects.equals(userDto.id(), that.userDto.id());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userDto.id());
+    }
+
 }
