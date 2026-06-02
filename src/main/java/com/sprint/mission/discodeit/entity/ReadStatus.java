e518e2abedd3284f34cab24dbfdb984174b16284
박린 <lynnpark2003@ehwa.ac.kr>
2026-06-02 16:46:34 +0900

    refactor: 채널에 새로운 메시지가 등록되었을 때 알림 생성하도록 리팩토링

--- src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
@@ -27,21 +27,28 @@ public class ReadStatus extends BaseUpdatableEntity {
   @ManyToOne(fetch = FetchType.LAZY, optional = false)
   @JoinColumn(name = "user_id", columnDefinition = "uuid")
   private User user;
+
   @ManyToOne(fetch = FetchType.LAZY, optional = false)
   @JoinColumn(name = "channel_id", columnDefinition = "uuid")
   private Channel channel;
+
   @Column(columnDefinition = "timestamp with time zone", nullable = false)
   private Instant lastReadAt;
 
-  public ReadStatus(User user, Channel channel, Instant lastReadAt) {
+  @Column(nullable = false)
+  private boolean notificationEnabled;
+
+  public ReadStatus(User user, Channel channel, Instant lastReadAt, boolean notificationEnabled) {
     this.user = user;
     this.channel = channel;
     this.lastReadAt = lastReadAt;
+    this.notificationEnabled = notificationEnabled;
   }
 
-  public void update(Instant newLastReadAt) {
+  public void update(Instant newLastReadAt, boolean notificationEnabled) {
     if (newLastReadAt != null && !newLastReadAt.equals(this.lastReadAt)) {
       this.lastReadAt = newLastReadAt;
+      this.notificationEnabled = notificationEnabled;
     }
   }
 }
