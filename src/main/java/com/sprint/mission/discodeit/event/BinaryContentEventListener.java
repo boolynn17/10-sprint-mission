5e8cc08b09113b5d441f121b9ca427256621aef3
박린 <lynnpark2003@ehwa.ac.kr>
2026-06-02 14:36:58 +0900

    refactor: 바이너리 데이터 저장 성공 여부 메타 데이터에 반영하도록 수정

--- src/main/java/com/sprint/mission/discodeit/event/BinaryContentEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/BinaryContentEventListener.java
@@ -1,5 +1,7 @@
 package com.sprint.mission.discodeit.event;
 
+import com.sprint.mission.discodeit.entity.BinaryContentStatus;
+import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,16 +9,29 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+import java.util.UUID;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class BinaryContentEventListener {
+
     private final BinaryContentStorage binaryContentStorage;
+    private final BinaryContentService binaryContentService;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleBinaryContentCreated(BinaryContentCreatedEvent event) {
+
+        UUID id = event.getBinaryContent().getId();
         log.debug("바이너리 데이터 저장 시작: id={}", event.getBinaryContent().getId());
-        binaryContentStorage.put(event.getBinaryContent().getId(), event.getBytes());
-        log.debug("바이너리 데이터 저장 완료: id={}", event.getBinaryContent().getId());
+
+        try {
+            binaryContentStorage.put(id, event.getBytes());
+            binaryContentService.updateStatus(id, BinaryContentStatus.SUCCESS);  // 성공
+            log.debug("바이너리 데이터 저장 완료: id={}", id);
+        } catch (Exception e) {
+            log.error("바이너리 데이터 저장 실패: id={}", id, e);
+            binaryContentService.updateStatus(id, BinaryContentStatus.FAIL);  // 실패
+        }
     }
 }
